aad3d789b1217702af4d4167dfeca02bb18a8c4d
Quoc Khanh <nguyenkimt322@gmail.com>
2023-11-03 16:31:36 +0700

    chore/register_function

--- src/helpers/hash.js
+++ src/helpers/hash.js
@@ -1,11 +1,19 @@
 const crypto = require('crypto');
 
-function hashPassword(plainPassword) {
-    const salt = crypto.randomBytes(16).toString('hex');
-    const hashedPassword = crypto.pbkdf2Sync(plainPassword, salt, 100, 64, 'sha512').toString('hex');
-    return { hashedPassword, salt };
+// function hashPassword(plainPassword) {
+//     const salt = crypto.randomBytes(16).toString('hex');
+//     const hashedPassword = crypto.pbkdf2Sync(plainPassword, salt, 100, 64, 'sha512').toString('hex');
+//     return { hashedPassword, salt};
+// }
+function hashPassword(password) {
+    const salt = crypto.randomBytes(16).toString('hex'); // Giới hạn salt chỉ có 32 ký tự
+    console.log(salt);
+    const hashedPw = crypto.pbkdf2Sync(password, salt, 10000, 64, 'sha512').toString('hex');
+    return {
+        salt,
+        hashedPw
+    }
 }
-
 function comparePassword(hashedPassword, salt, plainPassword) {
     const hashedRawPassword = crypto.pbkdf2Sync(plainPassword, salt, 100, 64, `sha512`).toString(`hex`);
     return hashedPassword===hashedRawPassword;
--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,7 @@ const router = express.Router();
 router.use('/user', require('./user'));
 router.use('/task', require('./task'));
 router.use('/manager', require('./manager'));
+router.use('/auth', require('./auth'));
 
 
 module.exports = router;
